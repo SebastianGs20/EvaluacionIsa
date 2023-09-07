0d3fd41984c1f1bcec3c0b316c7b08758bdd8250
SebastianGs20 <118822817+SebastianGs20@users.noreply.github.com>
2023-09-07 08:39:39 -0500

    Add files via upload

---
+++ ejercicio 2.js
@@ -0,0 +1,8 @@
+const sueldo = [1360000]
+const empleado = ['Andres']
+const bonificacion = [200000]
+
+for (let i = []; i = bonificacion; i++) {
+    const saldo_total = sueldo+i;
+console.log(empleado(saldo_total));
+}
