5f79847507e06cb2728db31937dca8c7bdd21ea8
SebastianGs20 <118822817+SebastianGs20@users.noreply.github.com>
2023-09-07 08:51:27 -0500

    Add files via upload

---
+++ ejercicio 3.js
@@ -0,0 +1,8 @@
+let numero= parseInt(prompt("señor usuario digite el numero a factoriar"))
+console.log(numero);
+for (let i = 0; i < numero; i++) {
+    if (numero < 0) {   
+        const factorial = numero *[2];
+        console.log(factorial);
+    }
+}
